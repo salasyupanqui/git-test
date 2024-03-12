c5a58aef28499d91344b44040cbe1a2e6bcc2d20
Jean Salas <jean99salas1@gmail.com>
2024-03-12 15:38:45 -0500

    primer commit

---
+++ hola.js
@@ -0,0 +1,12 @@
+//calcular porcentaje
+let valor1 = prompt("Dame el valor 1", 5);
+let valor2 = prompt("Dame el valor 2", 4);
+
+// Convertir los valores a enteros (si es necesario)
+valor1 = parseInt(valor1);
+valor2 = parseInt(valor2);
+
+// Realizar alguna operación con los valores (por ejemplo, suma)
+const suma = valor1 + valor2;
+
+console.log(`La suma de los valores es: ${suma}`);
